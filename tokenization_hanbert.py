5ad37eb0810ebaacbb83ac4cf0a4f27f06f456f5
monologg <adieujw@gmail.com>
2020-01-25 21:40:01 +0900

    Erase redundant code on tokenizer

--- tokenization_hanbert.py
+++ tokenization_hanbert.py
@@ -88,56 +88,6 @@ class MorAn16(object):
         return result
 
 
-def validate_case_matches_checkpoint(do_lower_case, init_checkpoint):
-    """Checks whether the casing config is consistent with the checkpoint name."""
-
-    # The casing has to be passed in by the user and there is no explicit check
-    # as to whether it matches the checkpoint. The casing information probably
-    # should have been stored in the bert_config.json file, but it's not, so
-    # we have to heuristically detect it to validate.
-
-    if not init_checkpoint:
-        return
-
-    m = re.match("^.*?([A-Za-z0-9_-]+)/bert_model.ckpt", init_checkpoint)
-    if m is None:
-        return
-
-    model_name = m.group(1)
-
-    lower_models = [
-        "uncased_L-24_H-1024_A-16", "uncased_L-12_H-768_A-12",
-        "multilingual_L-12_H-768_A-12", "chinese_L-12_H-768_A-12"
-    ]
-
-    cased_models = [
-        "cased_L-12_H-768_A-12", "cased_L-24_H-1024_A-16",
-        "multi_cased_L-12_H-768_A-12"
-    ]
-
-    is_bad_config = False
-    if model_name in lower_models and not do_lower_case:
-        is_bad_config = True
-        actual_flag = "False"
-        case_name = "lowercased"
-        opposite_flag = "True"
-
-    if model_name in cased_models and do_lower_case:
-        is_bad_config = True
-        actual_flag = "True"
-        case_name = "cased"
-        opposite_flag = "False"
-
-    if is_bad_config:
-        raise ValueError(
-            "You passed in `--do_lower_case=%s` with `--init_checkpoint=%s`. "
-            "However, `%s` seems to be a %s model, so you "
-            "should pass in `--do_lower_case=%s` so that the fine-tuning matches "
-            "how the model was pre-training. If this error is wrong, please "
-            "just comment out this check." % (actual_flag, init_checkpoint,
-                                              model_name, case_name, opposite_flag))
-
-
 def convert_to_unicode(text):
     """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
     if six.PY3:
@@ -152,29 +102,6 @@ def convert_to_unicode(text):
             return text.decode("utf-8", "ignore")
         elif isinstance(text, unicode):
             return text
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
-    else:
-        raise ValueError("Not running on Python2 or Python 3?")
-
-
-def printable_text(text):
-    """Returns text encoded in a way suitable for print or `tf.logging`."""
-
-    # These functions want `str` for both Python2 and Python3, but in one case
-    # it's a Unicode string and in the other it's a byte string.
-    if six.PY3:
-        if isinstance(text, str):
-            return text
-        elif isinstance(text, bytes):
-            return text.decode("utf-8", "ignore")
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
-    elif six.PY2:
-        if isinstance(text, str):
-            return text
-        elif isinstance(text, unicode):
-            return text.encode("utf-8")
         else:
             raise ValueError("Unsupported string type: %s" % (type(text)))
     else:
@@ -225,30 +152,6 @@ def whitespace_tokenize(text):
         return []
     tokens = text.split()
     return tokens
-
-
-class FullTokenizer(object):
-    """Runs end-to-end tokenziation."""
-
-    def __init__(self, vocab_file, do_lower_case=False, use_moran=False):
-        self.vocab = load_vocab(vocab_file)
-        self.inv_vocab = {v: k for k, v in self.vocab.items()}
-        self.basic_tokenizer = BasicTokenizer(use_moran=use_moran)
-        self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab)
-
-    def tokenize(self, text):
-        split_tokens = []
-        for token in self.basic_tokenizer.tokenize(text):
-            for sub_token in self.wordpiece_tokenizer.tokenize(token):
-                split_tokens.append(sub_token)
-
-        return split_tokens
-
-    def convert_tokens_to_ids(self, tokens):
-        return convert_by_vocab(self.vocab, tokens)
-
-    def convert_ids_to_tokens(self, ids):
-        return convert_by_vocab(self.inv_vocab, ids)
 
 
 class BasicTokenizer(object):
